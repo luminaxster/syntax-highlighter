c897bf00355aa1ad6e2ecc9a654122a8160c4312
David Samudio <luminaxster@gmail.com>
2021-03-02 16:21:54 -0500

    libraryTarget: 'commonjs2' =>  libraryTarget: 'umd'.  package.json up to date.

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,54 @@
-const path = require('path');
+const path  = require('path');
 
 module.exports = function (env) {
-    return {
-        mode: env.production ? 'production' : 'development',
-        devtool: env.production ? 'source-map' : 'eval',
-        entry: './src/index.js',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'index.js',
-            libraryTarget: 'commonjs2',
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/i,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader",
-                        options: {
-                            presets: [
-                                '@babel/preset-env',
-                                '@babel/preset-react',
-                            ],
-                            plugins: ['@babel/plugin-proposal-class-properties']
-                        }
-                    }
-                },
-                {
-                    test: /\.css$/i,
-                    use: ["style-loader", "css-loader"]
-                },
-                {
-                    test: /\.(png|jpg|gif)$/i,
-                    use: {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8192
-                        }
-                    }
-                }
-            ],
-        },
-        resolve: {
-            extensions: ['.js'],
-        },
-    }
+   const devMode = !env.production;
+   
+   return {
+      mode: devMode ? 'development' : 'production',
+      entry: './src/index.js',
+      output: {
+         path: path.resolve(__dirname, 'dist'),
+         filename: 'index.js',
+         libraryTarget: 'umd',
+      },
+      module: {
+         rules: [
+            {
+               test: /\.(js|jsx)$/i,
+               exclude: /node_modules/,
+               use: {
+                  loader: "babel-loader",
+                  options: {
+                     presets: [
+                        '@babel/preset-env',
+                        '@babel/preset-react',
+                     ],
+                     plugins: ['@babel/plugin-proposal-class-properties']
+                  }
+               }
+            },
+            {
+               test: /\.css$/i,
+               exclude: /node_modules/,
+               use: [
+                  'style-loader',
+                  'css-loader'
+               ]
+            },
+            {
+               test: /\.(png|jpg|gif)$/i,
+               exclude: /node_modules/,
+               use: {
+                  loader: 'url-loader',
+                  options: {
+                     limit: 8192
+                  }
+               }
+            }
+         ],
+      },
+      resolve: {
+         extensions: ['.js'],
+      },
+   }
 };
